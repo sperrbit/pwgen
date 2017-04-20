28faf407b867c51b2428f692138c2e7a6b05f30a
Chris <christoph.franke@outlook.com>
2017-04-20 13:30:55 +0200

    Update pwgen.py

--- pwgen.py
+++ pwgen.py
@@ -7,17 +7,24 @@ import string
 
 # Pasring the Arguments
 parser = argparse.ArgumentParser(description='LAN-SCAN')
-parser.add_argument('-l','--length', help='Password length', default=12)
+parser.add_argument('-l','--length', help='Password length', default=15)
 parser.add_argument('-q','--quantity', help='The number of passwords you want to be generated', default=1)
+parser.add_argument('-c','--complexity', help='Defines if you want special characters in your password', default=1)
 
 args = vars(parser.parse_args())
 length = str(args['length'])
 quantity = int(args['quantity'])
+complexity = int(args['complexity'])
 
 for x in range (0, quantity):
 
 	# Generating the password
-	chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+	
+	sc = '!@#$%&*()'
+	if complexity == 1:
+	    chars = string.ascii_letters + string.digits + sc 
+	else: 
+	    chars = string.ascii_letters + string.digits
+	
 	random.seed = (os.urandom(1024))
-
 	print ''.join(random.choice(chars) for i in range(int(length)))
